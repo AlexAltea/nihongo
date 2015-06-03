e83bb28b204e729e0384bf60fa1fd10077498677
Alexandro Sánchez Bach <alexandro.sanchez@stud.uni-heidelberg.de>
2015-06-03 23:43:31 +0200

    Setting up project environment

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load tasks from grunt-* dependencies in package.json
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take
+    require('time-grunt')(grunt);
+
+    // Project configuration
+    grunt.initConfig({
+        yeoman: {
+            app: 'app',
+            dist: 'dist'
+        },
+        copy: {
+            app: {
+                expand: true,
+                filter: 'isFile',
+                cwd: '<%= yeoman.app %>/',
+                src: ['**/*.html', '**/*.jpg'],
+                dest: '<%= yeoman.dist %>/'
+            }
+        },
+        wiredep: {
+            app: {
+                src: ['<%= yeoman.dist %>/index.html']
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    '<%= yeoman.dist %>/nihongo.css': [
+                        '<%= yeoman.app %>/styles/**/*.css'
+                    ]
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.dist %>',
+                    src: '{,*/}*.html',
+                    dest: '<%= yeoman.dist %>/'
+                }]
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    mangle: false
+                },
+                files: {
+                    '<%= yeoman.dist %>/nihongo.js': [
+                        '<%= yeoman.app %>/scripts/**/*.js'
+                    ]
+                }
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35728,
+                hostname: 'localhost',
+                base: '<%= yeoman.dist %>'
+            },
+            livereload: {
+                options: {
+                    open: true
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: '<%= yeoman.app %>/scripts/{,*/}*.ts',
+                tasks: ['typescript']
+            },
+            styles: {
+                files: '<%= yeoman.app %>/styles/{,*/}*.css',
+                tasks: ['cssmin']
+            },
+            views: {
+                files: '<%= yeoman.app %>/{,*/}*.html',
+                tasks: ['copy', 'wiredep', 'htmlmin']
+            },
+            livereload: {
+                files: [
+                    '<%= yeoman.dist %>/{,*/}*.html',
+                    '<%= yeoman.dist %>/*.js',
+                    '<%= yeoman.dist %>/*.css'
+                ],
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                }
+            },
+        }
+    });
+
+    // Project tasks
+    grunt.registerTask('test', [
+    ]);
+    grunt.registerTask('build', [
+        'copy',
+        'wiredep',
+        'cssmin',
+        'htmlmin',
+        'uglify'
+    ]);
+    grunt.registerTask('serve', [
+        'connect',
+        'watch'
+    ]);
+    grunt.registerTask('default', [
+        'test',
+        'build',
+        'serve'
+    ]);
+};
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# nihongo
-Random utilities to learn japanese
+Nihongo
+=====
+[![Last Release](https://badge.fury.io/gh/AlexAltea%2Fnihongo.svg)](https://github.com/AlexAltea/nihongo/releases)
+
+Random utilities to learn japanese.
+
+### Deploy
+To build the Nihongo client, clone the *master* branch of this repository, and do the following:
+
+**1.** Install the development and client dependencies:
+```
+npm install
+bower install
+```
+
+**2.** Finally, build the app with:
+```
+grunt build
+```
